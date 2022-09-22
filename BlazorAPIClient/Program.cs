8aa59369bdef3995891218ead7b063707274e8d0
hungnd2905 <hungnd2905@gmail.com>
2022-09-22 23:12:45 +0200

    Fetch Data RazorComponent

--- BlazorAPIClient/Program.cs
+++ BlazorAPIClient/Program.cs
@@ -1,11 +1,18 @@
-using BlazorAPIClient;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace BlazorAPIClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("BlazoreAPIClient has started...");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["api_base_url"]) });
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 .Net 6 Blazor API Client
   
 
+- SpaceX REST API:
 
+https://api.spacex.land/rest/launches
+https://api.spacex.land/rest/rockets
+
+- SpaceX GraphQL:
+
+https://api.spacex.land/graphql/
+
+- Tools:
+
+https://jsoneditoronline.org/
+https://app.quicktype.io
